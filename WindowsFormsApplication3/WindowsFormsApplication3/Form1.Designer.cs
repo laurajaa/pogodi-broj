1541b0512f0241d2a4476f73c46d78c5fdce1a7d
laurajaa <laurajakovljevic1603@gmail.com>
2026-05-20 14:23:55 +0200

    Add files via upload

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BrPokusaja = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Komentar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(146, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Unesi broj";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(263, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(115, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Broj pokušaja";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // BrPokusaja
+            // 
+            this.BrPokusaja.AutoSize = true;
+            this.BrPokusaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrPokusaja.Location = new System.Drawing.Point(267, 180);
+            this.BrPokusaja.Name = "BrPokusaja";
+            this.BrPokusaja.Size = new System.Drawing.Size(0, 25);
+            this.BrPokusaja.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(163, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Rezultat";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Komentar
+            // 
+            this.Komentar.AutoSize = true;
+            this.Komentar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Komentar.Location = new System.Drawing.Point(267, 132);
+            this.Komentar.Name = "Komentar";
+            this.Komentar.Size = new System.Drawing.Size(0, 25);
+            this.Komentar.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 276);
+            this.Controls.Add(this.Komentar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BrPokusaja);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Pogadjanje broja";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label BrPokusaja;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Komentar;
+    }
+}
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        int trazeniBroj;
+        int brojPokusaja = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // nasumičan broj od 1 do 100
+            trazeniBroj = rnd.Next(1, 101);
+
+            // kada se pritisne Enter u textboxu
+            textBox1.KeyDown += textBox1_KeyDown;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                int unetBroj;
+
+                // proverava da li je unet broj
+                if (int.TryParse(textBox1.Text, out unetBroj))
+                {
+                    brojPokusaja++;
+
+                    BrPokusaja.Text = brojPokusaja.ToString();
+
+                    if (unetBroj < trazeniBroj)
+                    {
+                        Komentar.Text = "Broj je veći!";
+                    }
+                    else if (unetBroj > trazeniBroj)
+                    {
+                        Komentar.Text = "Broj je manji!";
+                    }
+                    else
+                    {
+                        Komentar.Text = "Pogodili ste broj!";
+                        MessageBox.Show("Bravo! Pogodili ste broj za " +
+                        brojPokusaja + " pokušaja.");
+                    }
+
+                    textBox1.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Unesite ispravan broj!");
+                }
+            }
+        }
+    }
+}
